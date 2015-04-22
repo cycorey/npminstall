802a31fd4caf5406aa54dcad1dc0436485194628
Cao Yang <cycorey@gmail.com>
2015-04-22 13:00:59 +0800

    + some module

--- npm_install.cmd
+++ npm_install.cmd
@@ -1,2 +1,23 @@
 npm install is_js
 npm install lodash
+npm install bower
+npm install docker-browser-console
+npm install js-spark
+npm install webhdfs-proxy
+npm install webhdfs 
+npm install node-webhdfs
+npm install node-thrift-hbase
+npm install node-phoenix-proxy
+npm install hbase-client
+npm install hbase
+
+
+
+
+
+
+npm install -g inherits bower grunt
+npm install -g grunt-cli
+npm install -g docker
+
+npm install -g 1j01/ahk.js
